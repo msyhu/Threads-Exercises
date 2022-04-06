00eeea01bd3333b1cc21412d148765f71f128c97
msyhu <msyhu@korea.ac.kr>
2022-04-06 14:25:10 +0900

    ADD:create.c

---
+++ exercise/create.c
@@ -0,0 +1,22 @@
+#include<stdlib.h>
+#include<pthread.h>
+#include<stdio.h>
+
+#define NUM_THREADS 3
+
+void *hello_thread(void *arg) {
+    printf("%ld : Hello world!\n", (long)arg);
+}
+
+main() {
+    pthread_t tid[NUM_THREADS];
+    int i, status;
+    for (i = 0; i < NUM_THREADS; i++) {
+        pthread_create(&tid[i], NULL, hello_thread, (void *)i);
+    }
+    if (status != 0) {
+        printf("Thread creation failed: %d\n", status);
+        exit(1);
+    }
+    pthread_exit(NULL);
+}
